cb36a32e6ae7a4213e5233b6ba5b9864e74d968f
alpsayin <alpsayin@gmail.com>
2014-03-10 17:46:07 +0000

    fix a lot of bugs and add velocity&acceleration methods

--- AntennaRotor.m
+++ AntennaRotor.m
@@ -4,11 +4,11 @@ classdef AntennaRotor < handle
 %       Example:
 %           ar = AntennaRotor('COM1', 9600);
 %           ar.openPort();     
-%           ar.setup();
-%           ar.disableSafetyLimits();
 %           ar.resetSystem();
+%           ar.defaultSetup();
+%           ar.disableSafetyLimits();
 %           ar.setDegreesPerStep(2);
-%           ar.setDirection('cw');
+%           ar.setDirection('ccw');
 %           ar.activateStep();
 %           ar.close(); % never forget to close the port after you are done
 % 
@@ -17,24 +17,31 @@ classdef AntennaRotor < handle
 %                 obj = AntennaRotor(portName, baudrate)
 % 
 %       Functions:
-%                 setControllerAddress(address)
-%                 setup()
+%                 defaultSetup()
 %                 disableSafetyLimits()
 %                 enableSafetyLimits()
 %                 resetSystem()
 %                 setDegreesPerStep(degrees_per_step)
 %                 setDirection(direction)
+%                 setCW()
+%                 setCCW()
 %                 activateStep()
+%                 activateStepAndWait()
 %                 goToZero()
+%                 setControllerAddress(address)
+%                 setVelocity( revs_per_sec )
+%                 setAcceleration( revs_per_sec_sq )
 %                 emergencyStop()
 %                 openFile()
 %                 openPort()
 %                 close()
 %                 response = query(queryStr)
 %                 printf(varargin)
+%                 println(str)
 %                 response = scanf()
+% 
     properties(Constant)
-        PAUSE_TIME_PER_DEGREES_MS = 100;    % Time delay per degrees to wait for rotation to be completed
+        DEGREES_PER_MOTOR_REV = 1000;
     end
     properties(SetAccess = protected)
         degrees_per_step = 1;
@@ -45,6 +52,8 @@ classdef AntennaRotor < handle
         safetylimits = 1; %variable to hold the current state if safety limits enabled
         controlleraddress = 0; % got no idea what this address refers to (taken directly from original program)
         current_angle = 0; % current direction of the antenna
+        acceleration = 1;
+        velocity = 1;
     end
     methods
         function obj = AntennaRotor(portName, baudrate)
@@ -56,57 +65,83 @@ classdef AntennaRotor < handle
                 obj.baudrate = baudrate;
             end
         end
-        function setControllerAddress(obj, address)
-            obj.controllerAddress = address;
+        function delete(obj)
+            obj.close();
         end
-        function setup(obj)
-            obj.printf('A10\r\n');
-            obj.printf('V10\r\n');
-            obj.printf('D10000\r\n');
-            obj.printf('H-\r\n');
+        function defaultSetup(obj)
+            obj.setVelocity(10);
+            obj.setAcceleration(10);
+            obj.setDegreesPerStep(10);
+            obj.setCW();
         end
         function disableSafetyLimits(obj)
             obj.safetylimits = 0;
-            obj.printf('%dLD3\r\n');
+            obj.println('%dLD3', obj.controlleraddress);
         end
         function enableSafetyLimits(obj)
-            obj.printf('%dLD0\r\n', obj.controlleraddress);
+            obj.println('%dLD0', obj.controlleraddress);
             obj.safetylimits = 1;
         end
         function resetSystem(obj)
-            obj.printf('%dZ\r\n', obj.controlleraddress);
+            obj.println('%dZ', obj.controlleraddress);
         end
         function setDegreesPerStep(obj, degrees_per_step)
             obj.degrees_per_step = degrees_per_step;
-            obj.printf('D%d\r\n', obj.degrees_per_step*1000);
+            obj.println('D%d', obj.degrees_per_step*obj.DEGREES_PER_MOTOR_REV);
         end
         function setDirection(obj, direction)
             if strcmpi(direction,'cw')
-                obj.direction = 'cw';
-                obj.printf('H+\r\n');
+                obj.setCW();
             else
-                obj.direction = 'ccw';
-                obj.printf('H-\r\n');
+                obj.setCCW();
             end
         end
+        function setCW(obj)
+            obj.direction = 'cw';
+            obj.println('H+');
+        end
+        function setCCW(obj)
+            obj.direction = 'ccw';
+            obj.println('H-');
+        end
         function activateStep(obj)
-            obj.printf('G\r\n');
+            obj.println('G');
             if strcmpi(obj.direction, 'cw')
                 obj.current_angle = obj.current_angle + obj.degrees_per_step;
             else
                 obj.current_angle = obj.current_angle - obj.degrees_per_step;
             end
-            pause(obj.PAUSE_TIME_PER_DEGREES_MS/1000*obj.degrees_per_step)
+        end
+        function activateStepAndWait(obj)
+            obj.println('G');
+            if strcmpi(obj.direction, 'cw')
+                obj.current_angle = obj.current_angle + obj.degrees_per_step;
+            else
+                obj.current_angle = obj.current_angle - obj.degrees_per_step;
+            end
+            delay = obj.degrees_per_step/obj.velocity;
+            pause(delay)
         end
         function goToZero(obj)
-            obj.printf('H-\r\n');
-            obj.printf('%dLD0\r\n', obj.controlleraddress);
-            obj.printf('GH-2\r\n');
+            obj.println('H-');
+            obj.println('%dLD0', obj.controlleraddress);
+            obj.println('GH-2');
             obj.current_angle = 0;
         end
         function emergencyStop(obj)
-            obj.printf('MN\r\n')
-            obj.printf('K\r\n')
+            obj.println('MN')
+            obj.println('K')
+        end
+        function setControllerAddress(obj, address)
+            obj.controlleraddress = address;
+        end
+        function setVelocity(obj, revs_per_sec)
+            obj.velocity = revs_per_sec;
+            obj.println('V%d', obj.velocity);
+        end
+        function setAcceleration(obj, revs_per_sec_sq)
+            obj.acceleration = revs_per_sec_sq;
+            obj.println('A%d', obj.acceleration);
         end
         function openFile(obj)
         % debug method to test the protocol with files, shouldn't be used
@@ -126,6 +161,12 @@ classdef AntennaRotor < handle
         function printf(varargin)
             obj = varargin{1};
             fprintf(obj.comportObj, varargin{2:end});
+        end
+        function println(varargin)
+            obj = varargin{1};
+            str = sprintf(varargin{2:end});
+            fprintf(obj.comportObj, [str char(13) char(10)]);
+            fprintf([str char(13) char(10)]);
         end
         function response = scanf(obj)
             response = fscanf(obj.comportObj);
