539c0540a42d2096f7b0cb45c9d0c901500ec026
alpsayin <alpsayin@gmail.com>
2016-03-07 17:17:24 +0000

    add function to set echo mode

--- AntennaRotor.m
+++ AntennaRotor.m
@@ -243,6 +243,16 @@ classdef AntennaRotor < handle
             obj.acceleration = revs_per_sec_sq;
             obj.println('A%d', obj.acceleration);
         end
+		function setEchoMode(echo_mode_on)
+		%   setEchoMode()
+        %       Sets the echo mode of the controller. This class is not designed
+        %       to handle echoes from issued commands, therefore If echo mode is
+        %       to be turned on, it should be turned off after debugging.
+			if echo_mode_on
+				obj.println('%dSSA0', obj.controlleraddress);
+			else
+				obj.println('%dSSA1', obj.controlleraddress);
+		end
 		function requestLineFeed()
 		%   requestLineFeed()
         %       Sets the echo mode of the controller. This class is not designed
